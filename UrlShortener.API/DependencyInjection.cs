3e49296af787266a089d2bce3dd7f5a8c818af66
Leonardo Lima <leonardo.ferreira@techshop.com.br>
2025-11-19 15:19:21 -0300

    docs: descrição da API e inclusão do bearer token pelo swagger

--- UrlShortener.API/DependencyInjection.cs
+++ UrlShortener.API/DependencyInjection.cs
@@ -10,7 +10,48 @@ public static class DependencyInjection
     public static IServiceCollection AddWebApiServices(this IServiceCollection services)
     {
         services.AddEndpointsApiExplorer();
-        services.AddOpenApi();
+        services.AddOpenApi(options =>
+        {
+            options.AddDocumentTransformer((document, _, _) =>
+            {
+                document.Info = new Microsoft.OpenApi.Models.OpenApiInfo
+                {
+                    Title = "URL Shortener API",
+                    Version = "v1",
+                    Description = "API para encurtar URLs"
+                };
+                
+                document.Components ??= new Microsoft.OpenApi.Models.OpenApiComponents();
+
+                // Definição do esquema de segurança Bearer
+                document.Components.SecuritySchemes["Bearer"] = new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                {
+                    Type = Microsoft.OpenApi.Models.SecuritySchemeType.Http,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = Microsoft.OpenApi.Models.ParameterLocation.Header,
+                    Description = "Insira apenas o Bearer token (JWT)"
+                };
+
+                // Adiciona requisito global
+                document.SecurityRequirements.Add(new Microsoft.OpenApi.Models.OpenApiSecurityRequirement
+                {
+                    {
+                        new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+                        {
+                            Reference = new Microsoft.OpenApi.Models.OpenApiReference
+                            {
+                                Type = Microsoft.OpenApi.Models.ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+
+                return Task.CompletedTask;
+            });
+        });
         
         services.AddHttpContextAccessor();
         services.AddScoped<IUserContext, UserContext>();
